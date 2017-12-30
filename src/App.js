077da089e0a31da07b4d74cf8cd0b3a0bafcec2d
Siddharth Kothari <sids.aquarius@gmail.com>
2017-12-30 07:39:26 +0530

    adds styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
-import { ReactiveBase } from '@appbaseio/reactivesearch';
+import {
+  ReactiveBase,
+  DataSearch,
+  MultiDropdownList,
+  SingleRange,
+  SelectedFilters,
+  ResultCard
+} from '@appbaseio/reactivesearch';
 import './App.css';
 
 class App extends Component {
@@ -9,7 +16,79 @@ class App extends Component {
         app="good-books-live"
         credentials="sHZWU7AYJ:d1e2922c-035c-429f-bfe4-62aa38b1c395"
       >
-        Hello from ReactiveSearch!
+        <div className="navbar">
+          <div className="logo">
+            The Booksearch App
+          </div>
+          <DataSearch
+            className="datasearch"
+            componentId="mainSearch"
+            dataField={["original_title", "original_title.search", "authors", "authors.search"]}
+            queryFormat="and"
+            placeholder=" Search for books or authors"
+            innerClass={{
+              "input": "searchbox",
+              "list": "suggestionlist"
+            }}
+            filterLabel="search"
+          />
+        </div>
+        <div className={"display"}>
+          <div className={"leftSidebar"}>
+            <SingleRange
+              componentId="ratingsFilter"
+              dataField="average_rating_rounded"
+              title="Book Ratings"
+              data={[
+                { start: 4, end: 5, label: "★★★★ & up" },
+                { start: 3, end: 5, label: "★★★ & up" },
+                { start: 2, end: 5, label: "★★ & up" },
+                { start: 1, end: 5, label: "★ & up" },
+              ]}
+              react={{
+                and: "mainSearch"
+              }}
+              filterLabel="ratings"
+            />
+            <MultiDropdownList
+              componentId="publishFilter"
+              dataField="original_publication_year"
+              title="Published Year"
+              size={50}
+              placeholder="Filter"
+              showSearch={false}
+              showCheckbox={false}
+              showCount={false}
+              sortBy="desc"
+              filterLabel="published"
+            />
+          </div>
+          <div className={"mainBar"}>
+            <SelectedFilters />
+            <ResultCard
+              componentId="results"
+              dataField="original_title"
+              react={{
+                "and": ["mainSearch", "ratingsFilter", "publishFilter"]
+              }}
+              pagination={true}
+              size={8}
+              sortOptions={[
+                { dataField: "average_rating", sortBy: "desc", label: "Ratings (High to low)" },
+                { dataField: "original_title.raw", sortBy: "asc", label: "Title A->Z"},
+                { dataField: "original_title.raw", sortBy: "desc", label: "Title Z->A"}
+              ]}
+              onData={(res)=>(
+                {
+                  "image": res.image,
+                  "title": res.original_title,
+                  "desc":  res.average_rating + " ★ " +
+                  "<span style='float:right;margin-right:5px;'>Pub: " + res.original_publication_year + "</span><br/><br/>by " + res.authors
+                }
+              )}
+            />
+          </div>
+        </div>
       </ReactiveBase>
     );
   }
