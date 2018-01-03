5ea8e2a36def4c89c6c273c65ab9d531e6d094a3
Siddharth Kothari <sids.aquarius@gmail.com>
2018-01-03 07:32:40 +0530

    adds basic app implementation

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import React, { Component } from 'react';
 import {
   ReactiveBase,
   DataSearch,
-  MultiList,
-  RangeSlider,
   SingleRange,
-  SelectedFilters,
   ResultCard
 } from '@appbaseio/reactivesearch';
 import './App.css';
@@ -22,18 +19,16 @@ class App extends Component {
             The Booksearch App
           </div>
           <DataSearch
-            className="datasearch"
             componentId="mainSearch"
             dataField={["original_title", "original_title.search", "authors", "authors.search"]}
             queryFormat="and"
             placeholder="Search for a book title or an author"
+            autosuggest={false}
+            className="datasearch"
             innerClass={{
               "input": "searchbox",
               "list": "suggestionlist"
             }}
-            autosuggest={false}
-            iconPosition="left"
-            filterLabel="search"
           />
         </div>
         <div className={"display"}>
@@ -48,68 +43,22 @@ class App extends Component {
                 { start: 2, end: 5, label: "★★ & up" },
                 { start: 1, end: 5, label: "★ & up" },
               ]}
-              react={{
-                and: "mainSearch"
-              }}
-              filterLabel="Ratings"
-            />
-            <RangeSlider
-              componentId="publishFilter"
-              dataField="original_publication_year"
-              title="Year of Publication"
-              filterLabel="published"
-              range={{
-                start: 1970,
-                end: 2017
-              }}
-              rangeLabels={{
-                start: "1970",
-                end: "2017"
-              }}
-              interval={2}
-              react={{
-                and: "mainSearch"
-              }}
-            />
-            <MultiList
-              componentId="authorFilter"
-              dataField="authors.raw"
-              title="Authors"
-              size={1000}
-              showCheckbox={false}
-              className="authors"
-              innerClass={{
-                "list": "author-list"
-              }}
-              placeholder="Filter by author name"
-              filterLabel="Authors"
-              react={{
-                and: "mainSearch"
-              }}
             />
           </div>
           <div className={"mainBar"}>
-            <SelectedFilters />
             <ResultCard
               componentId="results"
               dataField="original_title"
               react={{
-                "and": ["mainSearch", "ratingsFilter", "publishFilter", "authorFilter"]
+                "and": ["mainSearch", "ratingsFilter"]
               }}
               pagination={true}
               size={8}
-              sortOptions={[
-                { dataField: "average_rating", sortBy: "desc", label: "Ratings (High to low)" },
-                { dataField: "original_title.raw", sortBy: "asc", label: "Title A->Z"},
-                { dataField: "original_title.raw", sortBy: "desc", label: "Title Z->A"}
-              ]}
               onData={(res)=>(
                 {
                   "image": res.image,
-                  "title": res.original_title || " ",
-                  "description":  res.average_rating + " ★ " +
-                  "<span style='float:right;margin-right:5px;'>Pub: " + res.original_publication_year + "</span><br/><br/><div class='result-author' title='" + res.authors + "'>by " + res.authors + "</div>",
-                  "url": "https://google.com/search?q=" + res.original_title
+                  "title": res.original_title,
+                  "description":  res.average_rating + " ★ "
                 }
               )}
               className="result-data"
